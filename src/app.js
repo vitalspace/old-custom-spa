9cd61c98d11dab1110d66f17b0164b0d3a275f4c
vitalspace <myglobalsounds@gmail.com>
2022-01-04 15:09:04 -0600

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const Web3 = require('web3');
+
+// import Web3 from "web3";
+
+// console.log(Web3)
+
+document.querySelector('#root').innerHTML = `
+  <p>This is a custom SPA.</p>
+`;
